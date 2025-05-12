fce26e6fbe41ea1375cdc48a0de0227fa33e89f7
franco <fr.fernandini@duocuc.cl>
2025-05-12 10:55:26 -0400

    ahora es posible registrar productos

--- msvc-productos/src/main/java/com/fernandini/msvc/productos/clients/ProveedorClientRest.java
+++ msvc-productos/src/main/java/com/fernandini/msvc/productos/clients/ProveedorClientRest.java
@@ -1,14 +1,15 @@
 package com.fernandini.msvc.productos.clients;
 
 
-import com.ecomarket.msvc_proveedores.models.Proveedor;
+
+import com.fernandini.msvc.productos.models.Proveedor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
-@FeignClient(name = "msvc-productos",url = "localhost:8082/api/v1/proveedores")
+@FeignClient(name = "msvc-productos",url = "localhost:8083/api/v1/proveedores")
 public interface ProveedorClientRest {
 
     @GetMapping
