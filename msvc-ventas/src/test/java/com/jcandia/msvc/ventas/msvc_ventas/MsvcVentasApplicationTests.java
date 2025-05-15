fb5e644fe0f410f75d452d83d8f024a90d326255
johao <joha.candia@duocuc.cl>
2025-05-14 21:03:49 -0400

    Creacion microservicio Ventas

---
+++ msvc-ventas/src/test/java/com/jcandia/msvc/ventas/msvc_ventas/MsvcVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jcandia.msvc.ventas.msvc_ventas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
