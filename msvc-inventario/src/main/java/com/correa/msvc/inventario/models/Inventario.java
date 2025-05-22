738236d3175ee6cb171fe1725d05c6b3b0d5f481
valparaiso <yet.correa@duocuc.cl>
2025-05-22 12:16:14 -0400

    inventarioService completo

--- msvc-inventario/src/main/java/com/correa/msvc/inventario/models/Inventario.java
+++ msvc-inventario/src/main/java/com/correa/msvc/inventario/models/Inventario.java
@@ -2,7 +2,10 @@ package com.correa.msvc.inventario.models;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
+
+import java.util.Date;
 
 @Entity
 @Getter @Setter @ToString @AllArgsConstructor
@@ -19,5 +22,24 @@ public class Inventario {
     @Column(name = "Cantidad-Inventario",nullable = false)
     @NotBlank(message = "El Campo de Cantidad no puede estar vacio")
     private String cantidadInventario;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_stock")
+    private Long stock_id;
+
+    @Column(name = "producto_fecha_ingreso",nullable = false)
+    @NotNull(message = "el campo de fecha de ingreso no puede estar vacio")
+    private Date fecha_ingreso_producto;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private Long idProducto;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_sucursal")
+    private Long stockSucursal;
 
 }
