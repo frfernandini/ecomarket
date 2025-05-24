f000faf6fb88d63bdb9c2dd9934229a27531e619
belcorr <yetsabelcorrea31@gmail.com>
2025-05-24 00:02:53 -0400

    controller,services Listos.

--- msvc-inventario/src/main/java/com/correa/msvc/inventario/repositories/InventarioRepository.java
+++ msvc-inventario/src/main/java/com/correa/msvc/inventario/repositories/InventarioRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface InventarioRepository extends JpaRepository<Inventario, Long> {
-    Optional<Inventario> findByIdInventario(Long idInventario);
+    Optional<Inventario> findById(Long idInventario);
 
 }
