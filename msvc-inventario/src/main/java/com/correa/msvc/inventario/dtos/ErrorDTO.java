08e69113cb3a11727fae2d0ba2f33668df66386a
belcorr <yetsabelcorrea31@gmail.com>
2025-05-23 23:33:54 -0400

    controller,services Listos.

---
+++ msvc-inventario/src/main/java/com/correa/msvc/inventario/dtos/ErrorDTO.java
@@ -0,0 +1,20 @@
+package com.correa.msvc.inventario.dtos;
+
+import java.util.Date;
+import java.util.Map;
+
+public class ErrorDTO {
+    private Integer status;
+    private Date date;
+
+    private Map<String,String>errors;
+
+    @Override
+    public String toString(){
+        return "{"+
+                "status="+status+
+                ",date="+date+
+                ",errors="+errors+
+                '{';
+    }
+}
---
+++ msvc-inventario/src/main/java/com/correa/msvc/inventario/dtos/InventarioDTO.java
@@ -0,0 +1,13 @@
+package com.correa.msvc.inventario.dtos;
+
+import java.util.Date;
+
+public class InventarioDTO {
+    private Long inventarioId;
+    private String cantidadInventario;
+    private Long stockId;
+    private Date fechaIngresoProducto;
+    private Long idProducto;
+    private Long stockSucursal;
+
+}
